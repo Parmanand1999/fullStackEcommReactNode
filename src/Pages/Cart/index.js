820ba4141967729b4902f2b1f793026d1c9ea945
Parmanand1999 <parmanandmaurya1999@gmail.com>
2025-01-04 15:42:14 +0530

    hhh

--- src/Pages/Cart/index.js
+++ src/Pages/Cart/index.js
@@ -1,4 +1,6 @@
+import { Rating } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   return (
@@ -16,7 +18,7 @@ const Cart = () => {
                 <table className="table">
                   <thead>
                     <tr>
-                      <th>Product</th>
+                      <th width="50%">Product</th>
                       <th>Unit Price</th>
                       <th>Quantity</th>
                       <th>Subtotal</th>
@@ -26,13 +28,19 @@ const Cart = () => {
                 </table>
                 <tbody>
                   <tr>
-                    <td>
+                    <td width="45%">
+                 
                       <div className="d-flex align-items-center cartItemimgWrapper">
                         <div className="imgWrapper">
-                          <img src="https://nest-frontend-v6.vercel.app/assets/imgs/shop/product-1-1.jpg" alt="" className="w-100" />
+                          <img
+                            src="https://nest-frontend-v6.vercel.app/assets/imgs/shop/product-1-1.jpg"
+                            alt=""
+                            className="w-100"
+                          />
                         </div>
-                        <div>
-                        <h6>Field Roast Chao Cheese Creamy Original</h6>
+                        <div className="info">
+                          <h6>Field Roast Chao Cheese Creamy Original</h6>
+                          <Rating name="read-onlay" value={4.5} readOnly precision={0.5} size="small"/>
                         </div>
                       </div>
                     </td>
